7bb4c612157cc6aec350506afc8ad589b01c9846
icchankun <ikkun.official@gmail.com>
2023-08-30 14:06:39 +0900

    usersテーブルを作成。

--- db/migrate/20230823025929_devise_create_users.rb
+++ db/migrate/20230823025929_devise_create_users.rb
@@ -32,6 +32,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :address,      null: false, default: ""
+      t.text   :introduction, null: false, default: ""
+      t.string :zip_code,     null: false, default: "", limit: 7
 
       t.timestamps null: false
     end
