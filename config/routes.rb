77dd4de7418925f52076fe06940e679f7a6bd48a
icchankun <ikkun.official@gmail.com>
2023-09-11 11:42:44 +0900

    プロフィールを編集した後の遷移先を変更。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root to: 'books#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   resources :books
   resources :users, only: %i[index show]
   
