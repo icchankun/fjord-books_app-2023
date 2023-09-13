4e3172373a782a6ba510e37edbd6d4aeb2785ebc
icchankun <ikkun.official@gmail.com>
2023-09-13 11:30:00 +0900

    アップロード可能なファイルをjpg, png, gifの3種類だけに限定。

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,15 @@ class User < ApplicationRecord
   has_one_attached :avatar do |attachable|
     attachable.variant :thumb, resize_to_limit: [100, 100]
   end
+
+  validate :correct_avatar_mime_type
+
+  private
+
+  def correct_avatar_mime_type
+    return unless avatar.attached? && !avatar.content_type.in?(%w[image/jpeg image/png image/gif])
+
+    avatar.purge
+    errors.add(:avatar, message: 'のファイル形式は、jpg, png, gifのいずれかにしてください。')
+  end
 end
