6de6b8cbef41af45a39a173a9e3cc7b6f8095a8b
icchankun <ikkun.official@gmail.com>
2023-10-23 12:38:34 +0900

    本にコメントを付けられるようにする。

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[destroy]
+  before_action :check_for_authorization, only: %i[destroy]
+
+  def create
+    @comment = current_user.comments.new(comment_params)
+
+    if @comment.save
+      redirect_to @comment.commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      instance_variable_set("@#{@comment.commentable_type.downcase}", @comment.commentable)
+      render "#{@comment.commentable_type.tableize}/show", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to @comment.commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+  end
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def check_for_authorization
+    redirect_to @comment.commentable unless current_user.id == @comment.user_id
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body, :commentable_id, :commentable_type)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
+
+  concern :commentable do
+    resources :comments, only: %i(create destroy)
+  end
+
+  resources :books, concerns: :commentable
   resources :users, only: %i(index show)
-  resources :reports
+  resources :reports, concerns: :commentable
 end
