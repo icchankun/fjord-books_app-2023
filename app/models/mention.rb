86fd5ff2ce1898d589f911491bd284b053295d64
icchankun <ikkun.official@gmail.com>
2023-11-27 12:46:46 +0900

    中間テーブルのカラムにバリデーションをかける。

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -3,4 +3,7 @@
 class Mention < ApplicationRecord
   belongs_to :mentioning, class_name: 'Report'
   belongs_to :mentioned, class_name: 'Report'
+
+  validates :mentioning_id, presence: true, uniqueness: { scope: :mentioned_id }
+  validates :mentioned_id, presence: true
 end
