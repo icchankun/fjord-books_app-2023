7c5b072b832909e1e315af7661eac5c97dbc51dd
icchankun <ikkun.official@gmail.com>
2023-12-04 10:40:23 +0900

    日報同士を多対多で関連づける。

--- app/models/report.rb
+++ app/models/report.rb
@@ -5,6 +5,8 @@ class Report < ApplicationRecord
   has_many :comments, as: :commentable, dependent: :destroy
   has_many :active_mentions, class_name: 'Mention', foreign_key: 'mentioning_id', inverse_of: 'mentioning', dependent: :destroy
   has_many :passive_mentions, class_name: 'Mention', foreign_key: 'mentioned_id', inverse_of: 'mentioned', dependent: :destroy
+  has_many :mentioning_reports, through: :active_mentions, source: :mentioned
+  has_many :mentioned_reports, through: :passive_mentions, source: :mentioning
 
   validates :title, presence: true
   validates :content, presence: true
