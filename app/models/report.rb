90c7513629cb31412000201c1a788c045c584ffb
icchankun <ikkun.official@gmail.com>
2023-12-04 10:37:00 +0900

    中間テーブルと日報テーブルを関連付ける。

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,8 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :active_mentions, class_name: 'Mention', foreign_key: 'mentioning_id', inverse_of: 'mentioning', dependent: :destroy
+  has_many :passive_mentions, class_name: 'Mention', foreign_key: 'mentioned_id', inverse_of: 'mentioned', dependent: :destroy
 
   validates :title, presence: true
   validates :content, presence: true
