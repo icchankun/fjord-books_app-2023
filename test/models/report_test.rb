a9615fbb8fb5449014210bf246d561db0c5a4f35
icchankun <ikkun.official@gmail.com>
2024-02-26 17:26:06 +0900

    Reportクラスのインスタンスメソッドの動作を確認するため、モデルテストを書く。

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,11 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'editable?' do
+    assert reports(:alice).editable?(users(:alice))
+  end
+
+  test 'created_on' do
+    assert_equal '2024-01-01', reports(:alice).created_on.to_s
+  end
 end
