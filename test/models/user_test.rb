d9386c6ae66de1ac429ecbdc898b6b4b0aefcd93
icchankun <ikkun.official@gmail.com>
2024-03-06 15:25:32 +0900

    fixtureの命名より名前が登録されているかどうかを判断できるようにする。

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,6 +5,6 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   test 'name_or_email' do
     assert_equal 'アリス', users(:alice).name_or_email
-    assert_equal 'bob@example.com', users(:bob).name_or_email
+    assert_equal 'nameless@example.com', users(:nameless).name_or_email
   end
 end
