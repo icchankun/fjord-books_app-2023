779ae2dcc8b32d706178dd0e00f10cc2d7a0bca2
icchankun <ikkun.official@gmail.com>
2024-04-30 15:52:44 +0900

    Userクラスのインスタンスメソッドに対してモデルテストを書く

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe '#name_or_email' do
+    context 'when a name is registered' do
+      it 'returns a name' do
+        alice = create(:user, :alice)
+        expect('アリス').to eq alice.name_or_email
+      end
+    end
+
+    context 'when a name is not registered' do
+      it 'returns a email' do
+        nameless = create(:user, :nameless)
+        expect('nameless@example.com').to eq nameless.name_or_email
+      end
+    end
+  end
+end
