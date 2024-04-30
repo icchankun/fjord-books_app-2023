ef7222b624959c22a4df6903c90a8f8c48da083c
icchankun <ikkun.official@gmail.com>
2024-04-30 15:53:00 +0900

    Reportクラスのインスタンスメソッドに対してモデルテストを書く

---
+++ spec/models/report_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Report, type: :model do
+  describe '#editable?' do
+    before do
+      @alice = create(:user, :alice)
+      @alice_report = create(:report, user: @alice)
+    end
+
+    context 'as a report author' do
+      it 'is editable' do
+        expect(@alice_report.editable?(@alice)).to be_truthy
+      end
+    end
+
+    context 'as a not reporting party' do
+      it 'is not editable' do
+        bob = create(:user, :bob)
+        expect(@alice_report.editable?(bob)).to be_falsey
+      end
+    end
+  end
+
+  describe '#created_on' do
+    it 'returns a date' do
+      report = create(:report)
+      expect(report.created_on).to eq Date.current
+    end
+  end
+end
